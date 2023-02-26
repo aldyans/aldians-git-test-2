0b918062363b56d1c644ce778c5080443f54bcbd
aldyans <writealdyans@gmail.com>
2023-02-26 12:37:03 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,8 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum aspernatur ad itaque modi minima perferendis non esse vero adipisci quia, facilis ullam nam, expedita aliquid. Hic reiciendis nihil esse deserunt?</p>
+    <ul>
+        <li>list</li>
+    </ul>
 </body>
 </html>
